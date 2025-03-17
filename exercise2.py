e89d9d23ab5dd482444e74e66327a22b6a7976cd
gague-UAM <ggpaiz@uamv.edu.ni>
2025-03-17 10:45:28 -0600

    Segundo ejercicio

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+number = 6
+city = "Masaya"
+price = 1.6
+is_single = True
+
+print(number, ",type",type(number))
+print(city, ",type",type(city))
+print(price, ",type",type(price))
+print(is_single, ",type",type(is_single))
+
+number = "seis"
+print(number, ",type",type(number))
