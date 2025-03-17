1f515ac7bbb821e4755d5e85f1145cc5025a4463
gague-UAM <ggpaiz@uamv.edu.ni>
2025-03-17 10:31:41 -0600

    Sesion 1 17 de Marzo

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+#Comentario de una linea
+"""
+Imprimir los dats personales del estudiante
+"""
+
+print("Datos del Estudiante")
+print("="*20)
+print("Nombre: ", end="")
+print("Gabriela Michelle")
+print("Apellidos: ", end="")
+print("Guerrero Paiz")
